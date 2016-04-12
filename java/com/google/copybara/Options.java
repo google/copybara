71a83356c69157ad0839fc11481b63d25e03185e
Miguel Alcon <malcon@google.com>
2016-04-12 11:45:27 -0400

    Fix ReplaceRegexTest
    Generics are difficult. This:
       ImmutableList.of(generalOptions)
    is resolved to ImmutableList<GeneralOption> no IL<Option>
    
    Also fixed Object usages in the class.
    
    Change-Id: Id7194e7c82927b3aef6cbb69ebaa949306068a67

--- java/com/google/copybara/Options.java
+++ java/com/google/copybara/Options.java
@@ -2,18 +2,16 @@ package com.google.copybara;
 
 import com.google.common.collect.ImmutableMap;
 
-import java.util.Arrays;
-
 /**
  * A class that groups all the options used in the program
  */
 public class Options {
 
-  private final ImmutableMap<Class<?>, Object> config;
+  private final ImmutableMap<Class<? extends Option>, Option> config;
 
-  public Options(Iterable<Option> options) {
-    ImmutableMap.Builder<Class<?>, Object> builder = ImmutableMap.builder();
-    for (Object option : options) {
+  public Options(Iterable<? extends Option> options) {
+    ImmutableMap.Builder<Class<? extends Option>, Option> builder = ImmutableMap.builder();
+    for (Option option : options) {
       builder.put(option.getClass(), option);
     }
     config = builder.build();
@@ -25,8 +23,8 @@ public class Options {
    * @throws IllegalStateException if the configuration cannot be found
    */
   @SuppressWarnings("unchecked")
-  public <T> T getOption(Class<? extends T> optionClass) {
-    Object config = this.config.get(optionClass);
+  public <T extends Option> T getOption(Class<? extends T> optionClass) {
+    Option config = this.config.get(optionClass);
     if (config == null) {
       throw new IllegalStateException("No option type found for " + optionClass);
     }
