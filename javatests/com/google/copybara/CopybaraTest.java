b615756c8874a852d7453e4beb9b4a8594c8727b
Matthew DeVore <matvore@google.com>
2016-04-25 12:42:03 -0700

    Add test method to make sure timestamp is read from Origin in CopybaraTest
    This also cleans up the testcase file and makes it easier to add more tests.
    
    Change-Id: I9f27b79ba879800c98607dc97df07f1e7e3ffb28

--- javatests/com/google/copybara/CopybaraTest.java
+++ javatests/com/google/copybara/CopybaraTest.java
@@ -6,6 +6,7 @@ import com.google.common.truth.Truth;
 import com.google.copybara.config.Config;
 import com.google.copybara.git.GitOptions;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -14,24 +15,25 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
 @RunWith(JUnit4.class)
 public class CopybaraTest {
 
-  private final static class CountTimesProcessedDestination implements Destination.Yaml {
-    int timesProcessed;
-    long beginTime = System.currentTimeMillis() / 1000;
+  private final static class RecordsProcessCallDestination implements Destination.Yaml {
+    List<Long> processTimestamps = new ArrayList<>();
 
     @Override
     public Destination withOptions(Options options) {
       return new Destination() {
         @Override
         public void process(Path workdir, String originRef, long timestamp, String changesSummary) {
-          timesProcessed++;
-          Truth.assertThat(timestamp).isAtLeast(beginTime);
-          Truth.assertThat(timestamp).isAtMost(System.currentTimeMillis() / 1000);
+          processTimestamps.add(timestamp);
         }
 
         @Nullable
@@ -43,54 +45,36 @@ public class CopybaraTest {
     }
   }
 
-  @Test
-  public void doNothing() throws Exception {
-    Config.Yaml config = new Config.Yaml();
-    config.setName("name");
-    config.setOrigin(new Origin.Yaml<DummyOrigin>() {
-      @Override
-      public DummyOrigin withOptions(Options options) {
-        return new DummyOrigin();
-      }
-    });
-
-    // Use a destination that only increments a counter when it is written.
-    CountTimesProcessedDestination destination = new CountTimesProcessedDestination();
-    config.setDestination(destination);
-    Path workdir = Files.createTempDirectory("workdir");
-    Options options = new Options(
-        ImmutableList.of(new GitOptions(), new GeneralOptions(workdir, /*verbose=*/true)));
-    new Copybara(workdir).runForSourceRef(config.withOptions(options), "some_sha1");
-    Truth.assertThat(Files.readAllLines(workdir.resolve("file.txt"), StandardCharsets.UTF_8))
-        .contains("some_sha1");
-    Truth.assertThat(destination.timesProcessed).isEqualTo(1);
-  }
-
-  private static class DummyOrigin implements Origin<DummyOrigin> {
+  private class DummyReference implements Origin.Reference<DummyOrigin> {
+    String reference;
 
     @Override
+    public Long readTimestamp() {
+      return referenceToTimestamp.get(reference);
+    }
+
+    @Override
+    public void checkout(Path workdir) throws RepoException {
+      try {
+        Files.createDirectories(workdir);
+        Files.write(workdir.resolve("file.txt"), reference.getBytes());
+      } catch (IOException e) {
+        throw new RepoException("Unexpected error", e);
+      }
+    }
+
+    @Override
+    public String asString() {
+      return reference;
+    }
+  }
+
+  private class DummyOrigin implements Origin<DummyOrigin> {
+    @Override
     public Reference<DummyOrigin> resolve(@Nullable final String reference) {
-      return new Reference<DummyOrigin>() {
-        @Override
-        public Long readTimestamp() {
-          return null;
-        }
-
-        @Override
-        public void checkout(Path workdir) throws RepoException {
-          try {
-            Files.createDirectories(workdir);
-            Files.write(workdir.resolve("file.txt"), reference.getBytes());
-          } catch (IOException e) {
-            throw new RepoException("Unexpected error", e);
-          }
-        }
-
-        @Override
-        public String asString() {
-          return reference;
-        }
-      };
+      DummyReference wrappedReference = new DummyReference();
+      wrappedReference.reference = reference;
+      return wrappedReference;
     }
 
     @Override
@@ -98,5 +82,59 @@ public class CopybaraTest {
         @Nullable Reference<DummyOrigin> newRef) throws RepoException {
       throw new CannotComputeChangesException("not supported");
     }
+  }
+
+  private Map<String, Long> referenceToTimestamp;
+  private RecordsProcessCallDestination destination;
+  private Config.Yaml yaml;
+  private Path workdir;
+
+  @Before
+  public void setup() throws Exception {
+    referenceToTimestamp = new HashMap<>();
+    destination = new RecordsProcessCallDestination();
+    yaml = new Config.Yaml();
+    yaml.setName("name");
+    yaml.setOrigin(new Origin.Yaml<DummyOrigin>() {
+      @Override
+      public DummyOrigin withOptions(Options options) {
+        return new DummyOrigin();
+      }
+    });
+
+    yaml.setDestination(destination);
+    workdir = Files.createTempDirectory("workdir");
+  }
+
+  private Options options() {
+    return new Options(
+        ImmutableList.of(new GitOptions(), new GeneralOptions(workdir, /*verbose=*/true)));
+  }
+
+
+  @Test
+  public void processIsCalledWithCurrentTimeIfTimestampNotInOrigin() throws Exception {
+    long beginTime = System.currentTimeMillis() / 1000;
+
+    new Copybara(workdir).runForSourceRef(yaml.withOptions(options()), "some_sha1");
+
+    long timestamp = destination.processTimestamps.get(0);
+    Truth.assertThat(timestamp).isAtLeast(beginTime);
+    Truth.assertThat(timestamp).isAtMost(System.currentTimeMillis() / 1000);
+  }
+
+  @Test
+  public void processIsCalledWithCorrectWorkdir() throws Exception {
+    new Copybara(workdir).runForSourceRef(yaml.withOptions(options()), "some_sha1");
+    Truth.assertThat(Files.readAllLines(workdir.resolve("file.txt"), StandardCharsets.UTF_8))
+        .contains("some_sha1");
+  }
+
+  @Test
+  public void sendsOriginTimestampToDest() throws Exception {
+    referenceToTimestamp.put("refname", (long) 42918273);
+    new Copybara(workdir).runForSourceRef(yaml.withOptions(options()), "refname");
+    Truth.assertThat(destination.processTimestamps.get(0))
+        .isEqualTo(42918273);
   }
 }
