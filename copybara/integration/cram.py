86cf928b18afbf88164a0d8eaf3261a31fa95ee1
Augie Fackler <augie@google.com>
2016-04-28 13:46:47 -0400

    cram: make a copy of the test script in TEST_TMPDIR
    This allows internal build to run these tests as well as bazel, so we can
    uncomment the cram_test rules here.
    
    Change-Id: I4a8690f01ab4a11ee4663dd253ef3cb8072e47ff

--- copybara/integration/cram.py
+++ copybara/integration/cram.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
-import sys
 import os
+import shutil
+import sys
+import tempfile
 
 def main(args):
     # paranoia: our script name is the same as the library we want
@@ -16,9 +18,19 @@ def main(args):
     from external import cram as fakecram
     # insert it into the start of sys.path
     sys.path.insert(0, os.path.dirname(fakecram.__file__))
+    from cram import _main
+    args = args[1:]
+    unused_opts, paths, unused_getusage = _main._parseopts(args)
+    td = tempfile.mkdtemp(dir=os.environ['TEST_TMPDIR'])
+    for p in paths:
+        dest = os.path.join(td, os.path.basename(p))
+        shutil.copyfile(p, dest)
+        for i, x in enumerate(args):
+            if x == p:
+                args[i] = dest
     # now we can import cram and be on our merry way
     import cram
-    sys.exit(cram.main(sys.argv[1:]))
+    sys.exit(cram.main(args))
 
 if __name__ == '__main__':
     main(sys.argv)
