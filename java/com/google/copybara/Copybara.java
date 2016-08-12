1e05a8511b2d5d86532aa1e29c9cf6db2b0156a9
Daniel Romero Sanchez <danielromero@google.com>
2016-08-12 14:04:57 -0400

    Reverts previous commit.
    Change-Id: Iee04fd1e7e457fddb2337d1af02441a33d257295

--- java/com/google/copybara/Copybara.java
+++ java/com/google/copybara/Copybara.java
@@ -2,8 +2,6 @@
 package com.google.copybara;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.StandardSystemProperty;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.copybara.config.Config;
@@ -16,14 +14,9 @@ import com.google.copybara.git.GerritOptions;
 import com.google.copybara.git.GitOptions;
 import com.google.copybara.transform.ValidationException;
 import com.google.copybara.util.console.Console;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.logging.LogManager;
 import javax.annotation.Nullable;
 
 /**
@@ -34,16 +27,13 @@ import javax.annotation.Nullable;
  */
 public class Copybara {
 
-  protected static final String COPYBARA_NAMESPACE = "com.google.copybara";
   protected static final ImmutableSet<Class<?>> BASIC_MODULES = ImmutableSet.<Class<?>>of(
       FolderDestination.Module.class);
 
   private final SkylarkParser skylarkParser;
-  private final Console console;
 
-  public Copybara(SkylarkParser skylarkParser, Console console) {
+  public Copybara(SkylarkParser skylarkParser) {
     this.skylarkParser = Preconditions.checkNotNull(skylarkParser);
-    this.console = Preconditions.checkNotNull(console);
   }
 
   protected List<Option> getAllOptions() {
@@ -76,6 +66,7 @@ public class Copybara {
     GeneralOptions generalOptions = options.get(GeneralOptions.class);
     Preconditions.checkArgument(!generalOptions.isValidate(), "Call validate() instead");
     Config config = skylarkParser.loadConfig(configContents, options);
+    Console console = generalOptions.console();
     console.progress("Validating configuration");
 
     validate(options, configContents, workflowName);
@@ -87,6 +78,7 @@ public class Copybara {
       throws RepoException, ValidationException, IOException, EnvironmentException {
     options.get(WorkflowOptions.class).setWorkflowName(workflowName);
     Config config = skylarkParser.loadConfig(configContent, options);
+    Console console = options.get(GeneralOptions.class).console();
     List<String> validationMessages = validateConfig(config);
     if (!validationMessages.isEmpty()) {
       console.error("Configuration is invalid:");
@@ -98,7 +90,7 @@ public class Copybara {
     }
   }
 
-  private void runConfigValidation(Config config) throws ConfigValidationException {
+  private void runConfigValidation(Config config, Console console) throws ConfigValidationException {
     List<String> validationMessages = validateConfig(config);
     if (!validationMessages.isEmpty()) {
       console.error("Configuration is invalid:");
@@ -117,45 +109,5 @@ public class Copybara {
     // TODO(danielromero): Move here SkylarkParser validations once Config has all the workflows.
     // checkCondition(!workflows.isEmpty(), ...)
     return ImmutableList.of();
-  }
-
-  void configureLogging(FileSystem fs) throws IOException {
-    Path baseDir = fs.getPath(getBaseLoggingDir());
-    Files.createDirectories(baseDir);
-    if (System.getProperty("java.util.logging.config.file") == null) {
-      LogManager.getLogManager().readConfiguration(new ByteArrayInputStream((
-          "handlers=java.util.logging.FileHandler\n"
-              + ".level=INFO\n"
-              + "java.util.logging.FileHandler.level=INFO\n"
-              + "java.util.logging.FileHandler.pattern="
-              + baseDir + "/copybara-%g.log\n"
-              + "java.util.logging.FileHandler.count=10\n"
-              + "java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter\n"
-              + "java.util.logging.SimpleFormatter.format="
-              + "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n")
-          .getBytes(StandardCharsets.UTF_8)
-      ));
-      console.info("Writing execution logs to: " + baseDir);
-    }
-  }
-
-  /**
-   * Returns the base directory to be used by Copybara to write execution logs.
-   *
-   * <p>Other implementations of {@link Copybara} can override the base logging directory.
-   */
-  protected String getBaseLoggingDir() {
-    String userHome = StandardSystemProperty.USER_HOME.value();
-    switch (StandardSystemProperty.OS_NAME.value()) {
-      case "Linux":
-        String xdgCacheHome = System.getenv("XDG_CACHE_HOME");
-        return Strings.isNullOrEmpty(xdgCacheHome)
-            ? userHome + "/.cache/" + COPYBARA_NAMESPACE
-            : xdgCacheHome + COPYBARA_NAMESPACE;
-      case "Mac OS X":
-        return userHome + "/Library/Logs/" + COPYBARA_NAMESPACE;
-      default:
-        return "/var/tmp/" + COPYBARA_NAMESPACE;
-    }
   }
 }
