2699755e6f620eae78973e1dade5948bb2544249
Henry Sudhof <hsudhof@google.com>
2016-10-05 18:30:35 -0400

    Remove unused import.
    Change-Id: I0ab080278f9d9f3b07b25633ab240f327e72c746

--- javatests/com/google/copybara/config/SkylarkParserTest.java
+++ javatests/com/google/copybara/config/SkylarkParserTest.java
@@ -52,7 +52,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Rule;
