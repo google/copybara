db333f315025652009bf7f14e38bc36c467917a1
Miguel Alcon <malcon@google.com>
2017-11-28 20:42:55 -0800

    Make the size of the GH API page public
    We need this internally to check if we potentially got more than one
    page of refs.
    
    PiperOrigin-RevId: 177260943
    Change-Id: I76549cca459b8fd483506bf503ca1ff35d10012d

--- java/com/google/copybara/git/github_api/GithubApi.java
+++ java/com/google/copybara/git/github_api/GithubApi.java
@@ -33,7 +33,7 @@ public class GithubApi {
   private final GitHubApiTransport transport;
   private final Profiler profiler;
 
-  private static final int REFS_PER_PAGE = 100;
+  public static final int REFS_PER_PAGE = 100;
 
   public GithubApi(GitHubApiTransport transport, Profiler profiler) {
     this.transport = Preconditions.checkNotNull(transport);
