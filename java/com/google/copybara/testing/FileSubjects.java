97c36ef167eca5b21d2c2d0c8473509d9ac2f90f
cpovirk <cpovirk@google.com>
2019-04-19 11:58:16 -0700

    Internal refactor
    PiperOrigin-RevId: 244397393
    Change-Id: Id183d95b28bf5c944b1ac7955b9e5ced37a35655

--- java/com/google/copybara/testing/FileSubjects.java
+++ java/com/google/copybara/testing/FileSubjects.java
@@ -85,7 +85,7 @@ public class FileSubjects {
       for (String filename : filenames) {
         Path filePath = getSubject().resolve(filename);
         if (Files.exists(filePath)) {
-          fail("does not have file", filePath);
+          failWithActual("expected not to have file", filePath);
         }
       }
     }
@@ -110,7 +110,7 @@ public class FileSubjects {
       for (String filename : dirs) {
         Path filePath = actual().resolve(filename);
         if (Files.isDirectory(filePath)) {
-          fail("does not have directory", filePath);
+          failWithActual("expected not to have directory", filePath);
         }
       }
       return this;
@@ -172,7 +172,7 @@ public class FileSubjects {
       Path targetPath = checkFile(target);
 
       if (!Files.isSymbolicLink(filePath)) {
-        fail("%s is not a Symlink", filename);
+        failWithActual("expected to be a Symlink", filename);
       }
       Path realTarget = filePath.resolveSibling(Files.readSymbolicLink(filePath));
       if (!Files.isSameFile(realTarget, targetPath)) {
@@ -185,25 +185,28 @@ public class FileSubjects {
      * Checks that there are no more files in the path.
      */
     public PathSubject containsNoMoreFiles() throws IOException {
-      Files.walkFileTree(getSubject(), new SimpleFileVisitor<Path>() {
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-          if (attrs.isRegularFile()) {
-            Path relativeFile = getSubject().relativize(file);
-            if (!whitelistedPaths.contains(relativeFile)) {
-              fail("contains no more files", relativeFile.toString());
+      Files.walkFileTree(
+          getSubject(),
+          new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                throws IOException {
+              if (attrs.isRegularFile()) {
+                Path relativeFile = getSubject().relativize(file);
+                if (!whitelistedPaths.contains(relativeFile)) {
+                  failWithActual("expected to contain no more files", relativeFile.toString());
+                }
+              }
+              return FileVisitResult.CONTINUE;
             }
-          }
-          return FileVisitResult.CONTINUE;
-        }
-      });
+          });
       return this;
     }
 
     private Path checkFile(String filename) {
       Path filePath = getSubject().resolve(filename);
       if (!Files.exists(filePath)) {
-        fail("has file", filePath);
+        failWithActual("expected to have file", filePath);
       }
       whitelistedPaths.add(getSubject().relativize(filePath));
       return filePath;
