0230ee28c17741e106083b0fd01978fc4727da8a
Josh Goldman <joshgoldman@google.com>
2022-08-02 13:52:43 -0700

    Add a utility class to wrap calls to diff3 merging tool
    BUG=213348659,241409625
    PiperOrigin-RevId: 464878597
    Change-Id: If7097ca8bb35c2cac1a48bf04c19d34701d708cb

---
+++ java/com/google/copybara/util/Diff3Util.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.copybara.util;
+
+import com.google.common.collect.Lists;
+import com.google.copybara.shell.Command;
+import com.google.copybara.shell.CommandException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Map;
+
+/** Diff utilities that shell out to diff3 commandline tool */
+final class Diff3Util {
+
+  private final String diff3Bin;
+  private final Map<String, String> environmentVariables;
+
+  public Diff3Util(String diff3Bin, Map<String, String> environmentVariables) {
+    this.diff3Bin = diff3Bin;
+    this.environmentVariables = environmentVariables;
+  }
+
+  public CommandOutputWithStatus diff(Path lhs, Path rhs, Path baseline, Path workDir)
+      throws CommandException, IOException {
+    // myfile oldfile yourfile
+    String mArg = "-m";
+    ArrayList<String> argv =
+        Lists.newArrayList(diff3Bin, lhs.toString(), baseline.toString(), rhs.toString(), mArg);
+    Command cmd =
+        new Command(
+            argv.toArray(new String[0]), environmentVariables, workDir.toFile());
+    CommandOutputWithStatus output;
+    try {
+      output = new CommandRunner(cmd).withVerbose(true).execute();
+    } catch (BadExitStatusWithOutputException e) {
+      throw new IOException("Error executing 'diff3'", e);
+    }
+    return output;
+  }
+}
---
+++ javatests/com/google/copybara/util/Diff3UtilTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.copybara.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class Diff3UtilTest {
+
+  @Rule public final TemporaryFolder tmpFolder = new TemporaryFolder();
+  private Path left;
+  private Path right;
+  private Path baseline;
+  private Path workdir;
+  Diff3Util underTest;
+
+  @Before
+  public void setUp() throws Exception {
+    Path rootPath = tmpFolder.getRoot().toPath();
+    left = createDir(rootPath, "left");
+    right = createDir(rootPath, "right");
+    baseline = createDir(rootPath, "baseline");
+    workdir = createDir(rootPath, "workdir");
+    underTest = new Diff3Util("/usr/bin/diff3", null);
+  }
+
+  @Test
+  public void simpleMergeSuccessTest() throws Exception {
+    writeFile(baseline, "baseline.txt", "a\nb\nc");
+    writeFile(left, "left.txt", "foo\na\nb\nc");
+    writeFile(right, "right.txt", "a\nb\nc\nbar");
+
+    CommandOutputWithStatus output =
+        underTest.diff(
+            left.resolve("left.txt"),
+            right.resolve("right.txt"),
+            baseline.resolve("baseline.txt"),
+            workdir);
+
+    String mergedFile = "foo\na\nb\nc\nbar";
+    assertThat(output.getStdout()).isEqualTo(mergedFile);
+  }
+
+  private Path createDir(Path parent, String name) throws IOException {
+    Path path = parent.resolve(name);
+    Files.createDirectories(path);
+    return path;
+  }
+
+  private void writeFile(Path parent, String fileName, String fileContents) throws IOException {
+    Path filePath = parent.resolve(fileName);
+    Files.createDirectories(filePath.getParent());
+    Files.writeString(parent.resolve(filePath), fileContents);
+  }
+}
