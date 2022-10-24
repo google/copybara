google/copybara

commit 302d42930fce6182afa5ac17fe7e89e41f1537b9

Two performance fixes for config loading
  - Resolve dependencies in batch instead of one by one. This allows
    implementations to perform a more efficient fetch of the
    dependencies.

  - Cache author parsing. This can be a problem in large organizations
    with lot of users in Starlark and where config is loaded lot of
    times (e.g. using --read-config-from-change)

PiperOrigin-RevId: 482675085
Change-Id: I4c36df12628185d9994281062558c3a29b1ab3b3

java/com/google/copybara/config/SkylarkParser.java
@@ -17,7 +17,7 @@
 package com.google.copybara.config;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.copybara.exception.ValidationException.checkCondition;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
@@ -225,15 +225,22 @@ public class SkylarkParser {
         for (SyntaxError error : ex.errors()) {
           console.error(error.toString());
         }
-        checkCondition(false, "Error loading config file.");
-        return null; // unreachable
+        throw new ValidationException("Error loading config file.");
       }
 
       // process loads
       Map<String, Module> loadedModules = new HashMap<>();
-      for (String load : prog.getLoads()) {
-        Module loadedModule = eval(content.resolve(load + BARA_SKY));
-        loadedModules.put(load, loadedModule);
+      ImmutableMap<String, String> fileToLoad = prog.getLoads().stream()
+              .collect(toImmutableMap(
+                      l -> l + BARA_SKY,
+                      l -> l));
+
+      for (Entry<String, ConfigFile> entry :
+          content
+              // Resolve all in one call so that the implementor can do it in batch/parallel.
+              .resolveAll(fileToLoad.keySet()).entrySet()) {
+        Module loadedModule = eval(entry.getValue());
+        loadedModules.put(fileToLoad.get(entry.getKey()), loadedModule);
       }
 
       // execute

java/com/google/copybara/authoring/AuthorParser.java
@@ -17,8 +17,13 @@
 package com.google.copybara.authoring;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.re2j.Matcher;
 import com.google.re2j.Pattern;
+import java.util.concurrent.ExecutionException;
 
 /**
  * A parser for the standard author format {@code "Name <email>"}.
@@ -34,11 +39,32 @@ public class AuthorParser {
   private static final Pattern IN_QUOTES =
       Pattern.compile("(\".+\")|(\'.+\')");
 
+  private static final LoadingCache<String, Author> CACHE =
+      CacheBuilder.newBuilder()
+          .maximumSize(1000000)
+          .build(
+              new CacheLoader<String, Author>() {
+                @Override
+                public Author load(String key) throws Exception {
+                  return internalParse(key);
+                }
+              });
   /**
    * Parses a Git author {@code string} into an {@link Author}.
    */
   public static Author parse(String author) throws InvalidAuthorException {
     Preconditions.checkNotNull(author);
+    try {
+      // Use a cache since repetitive load (thru --read-config-from-change) configs that
+      // define authors have a penalty because of the regex check/group.
+      return CACHE.get(author);
+    } catch (ExecutionException e) {
+      Throwables.propagateIfPossible(e.getCause(), InvalidAuthorException.class);
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private static Author internalParse(String author) throws InvalidAuthorException {
     if (IN_QUOTES.matcher(author).matches()) {
       author = author.substring(1, author.length() - 1); //strip quotes
     }

java/com/google/copybara/config/CapturingConfigFile.java
@@ -46,6 +46,17 @@ class CapturingConfigFile implements ConfigFile {
   }
 
   @Override
+  public ImmutableMap<String, ConfigFile> resolveAll(Set<String> paths) throws CannotResolveLabel {
+    ImmutableMap.Builder<String, ConfigFile> result = ImmutableMap.builder();
+    for (Map.Entry<String, ConfigFile> e : wrapped.resolveAll(paths).entrySet()) {
+      CapturingConfigFile capturingConfigFile = new CapturingConfigFile(e.getValue());
+      result.put(e.getKey(), capturingConfigFile);
+      children.add(capturingConfigFile);
+    }
+    return result.buildOrThrow();
+  }
+
+  @Override
   public String path() {
     return wrapped.path();
   }

java/com/google/copybara/config/ConfigFile.java
@@ -18,9 +18,11 @@ package com.google.copybara.config;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.copybara.exception.CannotResolveLabel;
 import com.google.copybara.util.FileUtil;
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * An object representing a configuration file and that it can be used to resolve
@@ -51,6 +53,21 @@ public interface ConfigFile {
    * @throws CannotResolveLabel if the path cannot be resolved to a content
    */
   ConfigFile resolve(String path) throws CannotResolveLabel;
+
+  /**
+   * Resolve a set of configs paths in a batch. This can by implementors to check existence or
+   * preload the bytes of the content in batch/parallel.
+   * @param paths a set of paths
+   * @return a map from paths to {@link ConfigFile}
+   * @throws CannotResolveLabel if any of the paths cannot be resolved
+   */
+  default ImmutableMap<String, ConfigFile> resolveAll(Set<String> paths) throws CannotResolveLabel {
+    ImmutableMap.Builder<String , ConfigFile> result = ImmutableMap.builder();
+    for (String path : paths) {
+      result.put(path, resolve(path));
+    }
+    return result.buildOrThrow();
+  }
 
   /**
    * Resolved, non-relative name of the config file.

commit f51e2a9203eca1671992cdc8bf2adab64342121d

Use distinct when looking for dependencies
This fixes a regression introduced in 302d4293 when a user does:

   load("foo", "one")
   load("foo", "two")

instead of:
   load("foo", "one", "two")

The change assumed that the load statements were unique by config
but obviously it is not the case.

This change has the added benefit (with 302d4293) that it will
load that config once.

PiperOrigin-RevId: 483422566
Change-Id: Ic481fd6fed70afd66ae49fa5330bfeb5c2a19a18

java/com/google/copybara/config/SkylarkParser.java
@@ -231,6 +231,7 @@ public class SkylarkParser {
       // process loads
       Map<String, Module> loadedModules = new HashMap<>();
       ImmutableMap<String, String> fileToLoad = prog.getLoads().stream()
+              .distinct()
               .collect(toImmutableMap(
                       l -> l + BARA_SKY,
                       l -> l));

javatests/com/google/copybara/config/SkylarkParserTest.java
@@ -55,6 +55,7 @@ import net.starlark.java.annot.StarlarkBuiltin;
 import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.eval.EvalException;
 import net.starlark.java.eval.NoneType;
+import net.starlark.java.eval.StarlarkInt;
 import net.starlark.java.eval.StarlarkValue;
 import org.junit.Before;
 import org.junit.Test;
@@ -350,6 +351,23 @@ public class SkylarkParserTest {
     String subfolderVal = parser.eval("subfolder_val", content);
     assertThat(subfolderVal).isEqualTo("subfolder_foo");
     assertThat(val).isEqualTo("main_foo");
+  }
+
+  /**
+   * Test load same file twice
+   */
+  @Test
+  public void testLoadSameFileTwice() throws Exception {
+    parser.addConfigFile("foo.bara.sky", ""
+            + "one = 1\n"
+            + "two = 2\n");
+
+    String content = ""
+        + "load('foo', 'one')\n"
+        + "load('foo', 'two')\n"
+        + "val = one + two\n"
+        + "\n";
+    assertThat(parser.<StarlarkInt>eval("val", content)).isEqualTo(StarlarkInt.of(3));
   }
 
   @Test