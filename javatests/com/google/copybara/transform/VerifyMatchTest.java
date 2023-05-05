google/copybara

commit 5e9ba586ce6cba31bbabdc4e4f4586d44843712d

Line number instead of char for verify no match
Currently we report the character number, that is not easy to find in
the file. Instead report the line number.

Change-Id: Idf981819eea0f5eb7fda4b9e11ea9d6e45e984d7

javatests/com/google/copybara/transform/VerifyMatchTest.java
@@ -139,7 +139,24 @@ public final class VerifyMatchTest {
         .onceInLog(
             MessageType.ERROR,
             ".*Error validating 'verify_match 'foo'': file1.txt - Unexpected match found at"
-                + " char 0 - 'foo'..*");
+                + " line 1 - 'foo'..*");
+  }
+
+  @Test
+  public void testNoMatchMultiline() throws Exception {
+    VerifyMatch transformation = eval("core.verify_match(\n"
+        + "  regex = 'foo',\n"
+        + "  verify_no_match = True,\n"
+        + ")");
+    Path file1 = checkoutDir.resolve("file1.txt");
+    writeFile(file1, "bar\n\nfoo\nbar\nother\n");
+    assertThrows(ValidationException.class, () -> transform(transformation));
+    console
+        .assertThat()
+        .onceInLog(
+            MessageType.ERROR,
+            ".*Error validating 'verify_match 'foo'': file1.txt - Unexpected match found at"
+                + " line 3 - 'foo'..*");
   }
 
   @Test

java/com/google/copybara/transform/VerifyMatch.java
@@ -17,9 +17,9 @@
 package com.google.copybara.transform;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.copybara.LocalParallelizer;
 import com.google.copybara.LocalParallelizer.TransformFunc;
@@ -122,13 +122,15 @@ public final class VerifyMatch implements Transformation {
         if (Files.isSymbolicLink(file.getPath())) {
           continue;
         }
-        String originalFileContent = new String(Files.readAllBytes(file.getPath()), UTF_8);
+        String originalFileContent = Files.readString(file.getPath());
         Matcher matcher = batchPattern.matcher(originalFileContent);
         if (verifyNoMatch == matcher.find()) {
           String error = checkoutDir.relativize(file.getPath()).toString();
           if (verifyNoMatch) {
-            error += String.format(" - Unexpected match found at char %d - '%s'.\n",
-                matcher.start(), matcher.group());
+            int line = Splitter.on('\n')
+                .splitToList(originalFileContent.substring(0, matcher.start())).size();
+            error += String.format(" - Unexpected match found at line %d - '%s'.\n",
+                line, matcher.group());
           } else {
             error += " - Expected string was not present.\n";
           }

commit b1e96e483ebf94444cf976fde266f5267067adf3

Add a regression for invalid UTF-8 verify_match
ede21648c7c8f2c579098171858a1017bd59643 reverted a regression introduced
in 5e9ba586ce6cba31bbabdc4e4f4586d44843712d because the function we use
for reading the string from file in core.verify_match needs to be able
to read non-utf8 files due to compatibility.

This adds a test for that case so that it doesn't happen again.

Change-Id: Ied2fe1dac9d91cf39f3965daaedb50f6143df11f

javatests/com/google/copybara/transform/VerifyMatchTest.java
@@ -63,6 +63,23 @@ public final class VerifyMatchTest {
     transform(transformation);
   }
 
+  /**
+   * We should be able to verify files that are not valid UTF-8 (e.g a PDF file because the user
+   * used glob(["**"]) without failing because of bad UTF-8.
+   */
+  @Test
+  public void testInvalidUTF8() throws Exception {
+    VerifyMatch transformation = eval(""
+        + "core.verify_match(\n"
+        + "  regex = 'foo',\n"
+        + "  verify_no_match = True,\n"
+        + ")");
+
+    Path file1 = checkoutDir.resolve("file1.txt");
+    Files.write(file1, new byte[]{(byte) 0xe2, 0x28, (byte) 0xa1});
+    transform(transformation);
+  }
+
   @Test
   public void testSimpleMatchVerifyNoMatchPasses() throws Exception {
     VerifyMatch transformation = eval("core.verify_match(\n"