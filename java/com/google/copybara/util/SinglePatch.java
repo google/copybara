878938e7a97c5c1efade13bbb9183611ce18f14c
Jeyoung Kwon <jeyoungkwon@google.com>
2023-08-31 12:44:00 -0400

    Consolidate SinglePatch and SinglePatchUtil
    Change-Id: I6d8db8c792e574caa8cee95512bfc61253d2e4e3

--- java/com/google/copybara/util/SinglePatch.java
+++ java/com/google/copybara/util/SinglePatch.java
@@ -19,14 +19,23 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.io.MoreFiles;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 
@@ -50,6 +59,35 @@ public class SinglePatch {
   public SinglePatch(ImmutableMap<String, String> fileHashes, byte[] diffContent) {
     this.fileHashes = fileHashes;
     this.diffContent = diffContent;
+  }
+
+  /**
+   * Create a SinglePatch object from two folders containing separate versions of the repository.
+   *
+   * <p>The location of the two folders matters. The SinglePatch includes a diff between the two
+   * folders which the parent of the destination will be used as the working directory for when
+   * created. The locations of the folders will affect the paths that appear in the diff output.
+   *
+   * @param destination is the version containing all the destination only changes.
+   * @param baseline is the version to diff against.
+   */
+  public static SinglePatch generateSinglePatch(Path destination, Path baseline,
+      HashFunction hashFunction, Map<String, String> environment)
+      throws IOException, InsideGitDirException {
+    ImmutableMap.Builder<String, String> hashesBuilder = ImmutableMap.builder();
+    Files.walkFileTree(destination, new SimpleFileVisitor<>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+          throws IOException {
+        HashCode hashCode = MoreFiles.asByteSource(file).hash(hashFunction);
+        hashesBuilder.put(destination.relativize(file).toString(),
+            new String(hashCode.asBytes(), UTF_8));
+        return FileVisitResult.CONTINUE;
+      }
+
+    });
+    byte[] diff = DiffUtil.diff(destination, baseline, false, environment);
+    return new SinglePatch(hashesBuilder.build(), diff);
   }
 
   private static String mustReadLine(BufferedReader reader) throws IOException {
--- javatests/com/google/copybara/util/SinglePatchTest.java
+++ javatests/com/google/copybara/util/SinglePatchTest.java
@@ -19,16 +19,135 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.hash.Hashing;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class SinglePatchTest {
 
+  @Rule
+  public final TemporaryFolder tmpFolder = new TemporaryFolder();
+  private Path workdir;
+  private Path baseline;
+  private Path destination;
+
   private static final byte[] emptyDiff = {};
   private static final ImmutableMap<String, String> emptyHashes = ImmutableMap.of();
+
+  @Before
+  public void setUp() throws IOException {
+    workdir = tmpFolder.getRoot().toPath();
+    baseline = Files.createDirectories(workdir.resolve("baseline"));
+    destination = Files.createDirectories(workdir.resolve("destination"));
+  }
+
+  @Test
+  public void testGenerateSinglePatch() throws IOException, InsideGitDirException {
+    SinglePatch singlePatch = SinglePatch.generateSinglePatch(destination, baseline,
+        Hashing.sha256(), System.getenv());
+    assertThat(singlePatch).isNotNull();
+  }
+
+  @Test
+  public void testGenerateSinglePatch_hashesFile() throws IOException, InsideGitDirException {
+    String testPath = "test/foo";
+    String testContents = "hello";
+    byte[] testBytes = testContents.getBytes(UTF_8);
+
+    Files.createDirectories(destination.resolve(testPath).getParent());
+    Files.write(destination.resolve(testPath), testBytes);
+
+    SinglePatch singlePatch = SinglePatch.generateSinglePatch(destination, baseline,
+        Hashing.sha256(), System.getenv());
+
+    assertThat(singlePatch.getFileHashes()).containsExactly(
+        testPath, new String(Hashing.sha256().hashBytes(testBytes).asBytes(), UTF_8)
+    );
+  }
+
+  @Test
+  public void testGenerateSinglePatch_diffsDirectories() throws IOException, InsideGitDirException {
+    String testPath = "test/foo";
+    String testDestinationContents = "hello\n";
+    write(destination, testPath, testDestinationContents);
+
+    String testBaselineContents = "baseline\n";
+    write(baseline, testPath, testBaselineContents);
+
+    SinglePatch singlePatch = SinglePatch.generateSinglePatch(destination, baseline,
+        Hashing.sha256(), System.getenv());
+
+    assertThat(new String(singlePatch.getDiffContent(), UTF_8)).isEqualTo(""
+        + "diff --git a/destination/test/foo b/baseline/test/foo\n"
+        + "index ce01362..180b47c 100644\n"
+        + "--- a/destination/test/foo\n"
+        + "+++ b/baseline/test/foo\n"
+        + "@@ -1 +1 @@\n"
+        + "-hello\n"
+        + "+baseline\n");
+  }
+
+  @Test
+  public void testGenerateSinglePatch_diffsDirectories_multipleFiles()
+      throws IOException, InsideGitDirException {
+    String testPath = "test/foo";
+    String testPath2 = "test/bar";
+
+    write(destination, testPath, "destination test\n");
+    write(baseline, testPath, "baseline test\n");
+    write(destination, testPath2, "destination test 2\n");
+    write(baseline, testPath2, "baseline test 2\n");
+
+    SinglePatch singlePatch = SinglePatch.generateSinglePatch(destination, baseline,
+        Hashing.sha256(), System.getenv());
+
+    assertThat(new String(singlePatch.getDiffContent(), UTF_8)).isEqualTo(""
+        + "diff --git a/destination/test/bar b/baseline/test/bar\n"
+        + "index 8630a48..55208f4 100644\n"
+        + "--- a/destination/test/bar\n"
+        + "+++ b/baseline/test/bar\n"
+        + "@@ -1 +1 @@\n"
+        + "-destination test 2\n"
+        + "+baseline test 2\n"
+        + "diff --git a/destination/test/foo b/baseline/test/foo\n"
+        + "index 06c9033..fd50d5b 100644\n"
+        + "--- a/destination/test/foo\n"
+        + "+++ b/baseline/test/foo\n"
+        + "@@ -1 +1 @@\n"
+        + "-destination test\n"
+        + "+baseline test\n");
+  }
+
+  @Test
+  public void testGenerateSinglePatch_diffsDirectories_emptyDiff()
+      throws IOException, InsideGitDirException {
+    String testPath = "test/foo";
+    String testDestinationContents = "hello";
+    write(destination, testPath, testDestinationContents);
+
+    String testBaselineContents = "hello";
+    write(baseline, testPath, testBaselineContents);
+
+    SinglePatch singlePatch = SinglePatch.generateSinglePatch(destination, baseline,
+        Hashing.sha256(), System.getenv());
+
+    assertThat(new String(singlePatch.getDiffContent(), UTF_8)).isEqualTo("");
+  }
+
+  public void write(Path folder, String relativePath, String contents) throws IOException {
+    byte[] bytes = contents.getBytes(UTF_8);
+    Files.createDirectories(folder.resolve(relativePath).getParent());
+    Files.write(folder.resolve(relativePath), bytes);
+
+  }
 
   @Test
   public void testPatchContainsHeader() throws IOException {
